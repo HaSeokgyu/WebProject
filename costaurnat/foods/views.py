ee65634bba7791af16af930e2e3d23ecf90efc85
하석규 <gktjrrb22@naver.com>
2024-05-10 00:10:31 +0900

    django 연습

--- costaurnat/foods/views.py
+++ costaurnat/foods/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
-
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -9,6 +9,13 @@ def index(request):
     return render(request, 'foods/index.html', context=context)
 
 def food_detail(request, food):
-    context = {'name' : food}
+    context = dict()
+    if food == 'chicken' :
+        context['name'] = '코딩에 빠진 닭'
+        context['description'] = '주머니가 가벼운 당신의 마음까지 생각한 가격 !'
+        context['price'] = 12000
+        context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404('이런 음식은 없다구요!')
     return render(request, 'foods/detail.html', context=context)
 
